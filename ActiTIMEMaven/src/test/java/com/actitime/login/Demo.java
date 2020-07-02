6d6859ccde30d1577d5656bd851744c6447b0a6f
Raghav <Raghav@192.168.1.2>
2020-07-02 08:44:06 +0530

    My First Commit

---
+++ ActiTIMEMaven/src/test/java/com/actitime/login/Demo.java
@@ -0,0 +1,13 @@
+package com.actitime.login;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo {
+
+	@Test
+	public void run()
+	{
+		Reporter.log("Test Running successfully",true);
+	}
+}
